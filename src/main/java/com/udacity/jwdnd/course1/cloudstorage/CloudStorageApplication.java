e9af4889251b4c865f2e26769c4dcc6cb4655e5b
Veaceslav Grec <grec.vs@gmail.com>
2021-12-11 22:27:48 +0100

    Add installation comments

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudStorageApplication {
 
+	/**
+	 * brew tap spring-io/tap
+	 * brew install spring-boot
+	 *
+	 * ./mvnw spring-boot:run
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CloudStorageApplication.class, args);
 	}
