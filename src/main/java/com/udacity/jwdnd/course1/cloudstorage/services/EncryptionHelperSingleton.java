b351af4fbf2106bd257b45faef4d5f44b49d6aa3
andrinka <alexandrina.cosciuc@gmail.com>
2022-04-30 20:11:06 +0300

    singleton

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/EncryptionHelperSingleton.java
@@ -0,0 +1,63 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class EncryptionHelperSingleton {
+
+    private static EncryptionHelperSingleton helper;
+
+    public static EncryptionHelperSingleton getInstance(){
+        if (helper == null){
+            return new EncryptionHelperSingleton();
+        }
+
+        return helper;
+    }
+
+
+    public String generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return Base64.getEncoder().encodeToString(salt);
+    }
+
+    public String encryptValue(String data, String key) {
+        byte[] encryptedValue = null;
+
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            encryptedValue = cipher.doFinal(data.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
+                | UnsupportedEncodingException | IllegalBlockSizeException | BadPaddingException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+        return Base64.getEncoder().encodeToString(encryptedValue);
+    }
+
+    public String decryptValue(String data, String key) {
+        byte[] decryptedValue = null;
+
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            decryptedValue = cipher.doFinal(Base64.getDecoder().decode(data));
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException
+                | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            return null;
+        }
+
+        return new String(decryptedValue);
+    }
+}
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/EncryptionService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/EncryptionService.java
@@ -1,16 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Base64;
 
 @Service
 public class EncryptionService {
@@ -18,42 +8,14 @@ public class EncryptionService {
 
 
     public String generateSalt() {
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        random.nextBytes(salt);
-        return Base64.getEncoder().encodeToString(salt);
+       return EncryptionHelperSingleton.getInstance().generateSalt();
     }
 
     public String encryptValue(String data, String key) {
-        byte[] encryptedValue = null;
-
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            encryptedValue = cipher.doFinal(data.getBytes("UTF-8"));
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
-                | UnsupportedEncodingException | IllegalBlockSizeException | BadPaddingException e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
-
-        return Base64.getEncoder().encodeToString(encryptedValue);
+        return EncryptionHelperSingleton.getInstance().encryptValue(data, key);
     }
 
     public String decryptValue(String data, String key) {
-        byte[] decryptedValue = null;
-
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            decryptedValue = cipher.doFinal(Base64.getDecoder().decode(data));
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException
-                | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
-            return null;
-        }
-
-        return new String(decryptedValue);
+        return EncryptionHelperSingleton.getInstance().decryptValue(data,key);
     }
 }
