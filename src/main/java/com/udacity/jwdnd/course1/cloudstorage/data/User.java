a814e791fb3018251f999b120cee25e53502b438
andrinka <alexandrina.cosciuc@gmail.com>
2022-04-30 19:55:23 +0300

    builder with setters - revert

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/data/User.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/data/User.java
@@ -8,7 +8,7 @@ public class User {
     private String firstName;
     private String lastName;
 
-    private User(Integer userId, String username, String salt, String password, String firstName, String lastName) {
+    public User(Integer userId, String username, String salt, String password, String firstName, String lastName) {
         this.userId = userId;
         this.username = username;
         this.salt = salt;
@@ -21,20 +21,40 @@ public class User {
         return userId;
     }
 
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getSalt() {
         return salt;
     }
 
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
